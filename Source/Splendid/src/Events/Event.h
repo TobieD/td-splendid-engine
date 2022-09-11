e96fd661291a2828ef57038ef0a44a8caf46d9b3
tobie.devries <tobiedevries@gmail.com>
2022-09-11 11:04:30 +0200

    add event dispatcher

--- Source/Splendid/src/Events/Event.h
+++ Source/Splendid/src/Events/Event.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "../Core/Core.h"
+#include "Splendid/Core.h"
 
 #include "EventType.h"
 #include "EventCategory.h"
@@ -14,13 +14,14 @@ namespace Splendid
 
 	#define EVENT_CLASS_CATEGORY(category)  virtual int GetCategoryFlags() const override { return category;}
 
-	#define EVENT_CLASS_TYPE(type)  static EventType GetStaticType() const override { return EventType::#type; }
-									virtual const char* GetName() const override { return #type;}
+	#define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::#type; }
+									virtual const char* GetName() const override { return #type; }
 									virtual EventType GetEventType() const override { return GetStaticType(); }
 
 
 	class SPLENDID_API Event
 	{
+		friend class EventDispatcher;
 	public:
 		virtual EventType GetEventType() const = 0;
 		virtual int GetCategoryFlags() const = 0;
@@ -37,4 +38,34 @@ namespace Splendid
 		bool m_Handled = false;
 
 	};
+
+	class EventDispatcher
+	{
+		template<typename T>
+		using EventFn = std::function<bool(T&)>;
+
+	public:
+		EventDispatcher(Event& event)
+			: m_Event(event) {}
+
+		template<typename T>
+		bool Dispatch(EventFn<T> func)
+		{
+			if (m_Event.GetEventType() == T::GetStaticType())
+			{
+				m_Event.m_Handled = func(*(T*)&m_Event);
+			}
+
+			return false;
+		}
+
+	private:
+		Event& m_Event;
+
+	};
+
+	inline std:ostream & operator<<(std::ostream& os, const Event& e)
+	{
+		return os << e.ToString();
+	}
 }
