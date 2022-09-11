38c6f7e3ac880f26b0b9d731a340c5e75a2e88ce
tobie.devries <tobiedevries@gmail.com>
2022-09-11 10:33:40 +0200

    add key events implementations

--- Source/Splendid/src/Events/Event.h
+++ Source/Splendid/src/Events/Event.h
@@ -12,22 +12,29 @@ namespace Splendid
 	//Events are currently blocking, meaning if an event occurs it will get dispatched and must be dealt with immediatly
 	//It will be better to buffer events and process them together in an update stage
 
+	#define EVENT_CLASS_CATEGORY(category)  virtual int GetCategoryFlags() const override { return category;}
+
+	#define EVENT_CLASS_TYPE(type)  static EventType GetStaticType() const override { return EventType::#type; }
+									virtual const char* GetName() const override { return #type;}
+									virtual EventType GetEventType() const override { return GetStaticType(); }
+
+
 	class SPLENDID_API Event
 	{
-		public:
-			virtual EventType GetEventType() const = 0;
-			virtual int GetCategoryFlags() const = 0;
-			virtual const char* GetName() const = 0;
+	public:
+		virtual EventType GetEventType() const = 0;
+		virtual int GetCategoryFlags() const = 0;
+		virtual const char* GetName() const = 0;
 
-			virtual std::string ToString() const { return GetName(); }
+		virtual std::string ToString() const { return GetName(); }
 
-			inline bool IsInCategory(EventCategory category)
-			{
-				return GetCategoryFlags() & category;
-			}
+		inline bool IsInCategory(EventCategory category)
+		{
+			return GetCategoryFlags() & category;
+		}
 
-		protected:
-			bool m_Handled = false;
+	protected:
+		bool m_Handled = false;
 
 	};
 }
--- Source/Splendid/src/Events/KeyEvent.h
+++ Source/Splendid/src/Events/KeyEvent.h
@@ -1,1 +1,63 @@
 #pragma once
+
+#include "Event.h"
+#include "EventType.h"
+#include "EventCategory.h"
+
+#include <sstream>
+
+namespace Splendid
+{
+	class SPLENDID_API KeyEvent : public Event
+	{
+	public:
+		inline int GetKeyCode() const { return m_KeyCode; }
+
+		EVENT_CLASS_CATEGORY(Keyboard | Input)
+
+	protected:
+		KeyEvent(int keyCode)
+			:m_KeyCode(keyCode) {}
+
+		int m_KeyCode;
+
+	};
+
+	class SPLENDID_API KeyPressedEvent : public KeyEvent
+	{
+	public:
+		KeyPressedEvent(int keyCode, int repeatCount)
+			: KeyEvent(keyCode), m_RepeatCount(repeatCount) { }
+
+		inline int GetRepeatCount() const { return m_RepeatCount; }
+
+		std::string ToString() const override
+		{
+		std:stringstream ss;
+			ss << "KeyPressedEvent: " << m_KeyCode << " (" << m_RepeatCount << " repeats)";
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyPressed);
+
+	private:
+		int m_RepeatCount;
+
+	};
+
+	class SPLENDID_API KeyReleasedEvent : public KeyEvent
+	{
+	public:
+		KeyReleasedEvent(int keyCode)
+			: KeyEvent(keyCode)) { }
+
+		EVENT_CLASS_TYPE(KeyReleased);
+
+		std::string ToString() const override
+		{
+			std:stringstream ss;	
+			ss << "KeyReleasedEvent: " << m_KeyCode;;
+			return ss.str();
+		}
+	};
+}
