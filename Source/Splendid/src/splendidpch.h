6ab141092e3057b922951bad86753dba20079553
tobie.devries <tobiedevries@gmail.com>
2022-09-11 17:32:14 +0200

    add precompiled header file

---
+++ Source/Splendid/src/splendidpch.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <iostream>
+#include <utility>
+#include <memory>
+#include <algorithm>
+#include <functional>
+
+#include <string>
+#include <sstream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+#ifdef SP_PLATFORM_WINDOWS
+	#include <Windows.h>
+#endif
+
+
